08849e344ee7a9b9381eae57d5e7005fca705f95
kadir-zeren <k4dzeren@gmail.com>
2023-07-24 09:04:54 +0300

    V5

--- src/components/ContainerCard.jsx
+++ src/components/ContainerCard.jsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { data } from "../helpers/data";
+import PlayerCard from "./PlayerCard";
 const ContainerCard = () => {
+  const [search, setSearch] = useState("");
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+  const filteredData = data.filter((item) =>
+    item.name.toLowerCase().includes(search.trim().toLowerCase())
+  );
   return (
     <>
-      <Form.Control type="search" placeholder="Search Player..." />
+      <Form.Control
+        type="search"
+        placeholder="Search Player..."
+        onChange={handleChange}
+      />
       <Container className="p-3 rounded-4 card-container my-3">
         <Row xs={2} md={4} lg={6} className="justify-content-center  g-3">
-          <Col>1 of 2</Col>
-          <Col>2 of 2</Col>
-          <Col>3 of 3</Col>
+          {filteredData.map((player, i) => (
+            <PlayerCard key={i} {...player} />
+          ))}
         </Row>
       </Container>
     </>
