4a851d79a8f396c6d90763c331f60914fd5132a3
kadir-zeren <k4dzeren@gmail.com>
2023-07-24 09:19:51 +0300

    V6

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,21 +1,27 @@
+import React, { useState } from "react";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 const PlayerCard = ({ name, img, statistics }) => {
+  const [showImage, setShowImage] = useState(true);
   return (
     <Col>
-      <Card>
-        <Card.Img variant="top" src={img} height="350px" />
-        <ul>
-          {statistics.map((item, i) => (
-            <li
-              className="list-unstyled
+      <Card onClick={() => setShowImage(!showImage)}>
+        {showImage ? (
+          <Card.Img variant="top" src={img} height="350px" />
+        ) : (
+          <ul>
+            {statistics.map((item, i) => (
+              <li
+                className="list-unstyled
             h5 text-start"
-              key={i}
-            >
-              🏀 {item}
-            </li>
-          ))}
-        </ul>
+                key={i}
+              >
+                {""}
+                🏀 {item}
+              </li>
+            ))}
+          </ul>
+        )}
         <Card.Footer>
           <Card.Title>{name}</Card.Title>
         </Card.Footer>
